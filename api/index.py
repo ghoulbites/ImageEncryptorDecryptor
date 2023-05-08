86ecd33b83e7b0c1b195ca5cf542520a20435af8
ghoulbites <mikehanna31@gmail.com>
2023-05-08 15:39:46 +0400

    more small changes

--- api/index.py
+++ api/index.py
@@ -20,7 +20,7 @@ def about():
 def aesEncrypt():
     # Get key and file from request
     key = request.form['key']
-    file = request.files['file'].read()
+    file = request.files['image'].read()
 
     # Hash key to get a 256-bit key
     hashed_key = SHA256(key.encode()).digest()
@@ -62,3 +62,54 @@ def aesEncrypt():
     response = jsonify(response_data)
     response.headers.set('Content-Type', mimetype)
     return response
+
+def aesEncrypt():
+    # Get the image file and key from the request
+    image_file = request.files['image']
+    key = request.form['key']
+    
+    # Hash the key using SHA-256 and get the first 32 bytes as the AES key
+    hashed_key = hashlib.sha256(key.encode()).digest()[:32]
+    
+    # Read the image file and convert it to bytes
+    image_bytes = BytesIO(image_file.read())
+
+    # Open the image using Pillow and encrypt it with AES-GCM
+    with Image.open(image_bytes) as im:
+        # Get the file extension and determine the output format
+        file_ext = im.format.lower()
+        if file_ext == 'jpeg':
+            output_format = 'JPEG'
+        elif file_ext == 'png':
+            output_format = 'PNG'
+        else:
+            output_format = 'BMP'
+        
+        # Encrypt the image with AES-GCM
+        nonce = get_random_bytes(12)
+        cipher = AES.new(hashed_key, AES.MODE_GCM, nonce=nonce)
+        ciphertext, tag = cipher.encrypt_and_digest(pad(im.tobytes(), AES.block_size))
+
+        # Create a new image from the encrypted data
+        encrypted_image = Image.frombytes(im.mode, im.size, ciphertext)
+
+        # Create an in-memory file object for the encrypted image
+        output_buffer = BytesIO()
+        encrypted_image.save(output_buffer, format=output_format)
+        encrypted_image_bytes = output_buffer.getvalue()
+
+        # Set the appropriate MIME type based on the file extension
+        if file_ext == 'png':
+            mimetype = 'image/png'
+        elif file_ext == 'jpg' or file_ext == 'jpeg':
+            mimetype = 'image/jpeg'
+        else:
+            mimetype = 'application/octet-stream'
+
+    # Return the encrypted image and tag as a JSON response
+    response = {
+        'image': encrypted_image_bytes,
+        'tag': tag.hex()
+    }
+    response.headers.set('Content-Type', mimetype)
+    return jsonify(response)
